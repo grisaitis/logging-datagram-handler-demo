fdca49f9a8075f620a45b36beac8f52276b25899
William C Grisaitis <wgrisaitis@gmail.com>
2020-06-30 19:09:54 -0400

    assert that the message packet is complete

--- logging_udp/making_log_record.py
+++ logging_udp/making_log_record.py
@@ -9,7 +9,10 @@ import struct
 def make_log_record(data: bytes) -> LogRecord:
     # logger.info("making LogRecord from datagram packet")
     # logger.debug(f"datagram packet is {len(data)} bytes long")
-    # slen = struct.unpack(">L", data[0:4])[0]
+    slen = struct.unpack(">L", data[0:4])[0]
     # logger.debug(f"LogRecord pickle is {slen} bytes long")
-    obj = pickle.loads(data[4:])
+    log_record_pickle = data[4:]
+    if len(log_record_pickle) != slen:
+        raise RuntimeError("Datagram packet incomplete")
+    obj = pickle.loads(log_record_pickle)
     return logging.makeLogRecord(obj)
